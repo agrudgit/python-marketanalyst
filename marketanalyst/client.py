34c06a7344401045e3c688a91231de36ca7ecbe3
Himanshu347 <dhameliya.hp992@live.com>
2020-09-08 04:22:06 -0400

    changed getdata

--- marketanalyst/client.py
+++ marketanalyst/client.py
@@ -86,92 +86,47 @@ class client:
         except:
             raise ValueError(response.text)
 
-    def get_indicator_category_id(self,indicator="",indicator_category="",lookup=""):
-        params = {"format":"json"}
-        if indicator:
-            params["indicator"] = indicator
-        if indicator_category:
-            params["indicator_category"] = indicator_category
-        if lookup:
-            params["lookup"] = lookup
-        response = requests.get(self.base_url + "/get_indicator",params=params)
-        try:
-            indicator_data = response.json()
-            indicator_category_id = indicator_data[0]["indicator_category_id"]
-            return indicator_category_id
-        except:
-            raise ValueError(response.text)
-
-    def getdata(self,symbol,indicator,date_start,date_end,master_id="",indicator_id="",order="",no_cols=""):
+    def getdata(self,exchange="",security_type="",indicator_category="",date_start="",date_end="",master_id="",indicator_id=""):
+        if exchange == "" or exchange == None:
+            raise ValueError("Please provice valid exchange")
+        if security_type == "" or security_type == None:
+            raise ValueError("Please provice valid security_type")
+        if indicator_category == "" or indicator_category == None:
+            raise ValueError("Please provice valid indicator_category")
+        if date_start == "" or date_start == None:
+            raise ValueError("Please provice valid date_start")
+        if date_end == "" or date_end == None:
+            date_end = date_start
         self.validate_date(date_start)
         self.validate_date(date_end)
         start_date_object = datetime.datetime.strptime(date_start, '%Y-%m-%d,%H:%M:%S')
         end_date_object = datetime.datetime.strptime(date_end, '%Y-%m-%d,%H:%M:%S')
         if end_date_object < start_date_object:
             raise ValueError("end date is before start date")
+        params = {"format":"csv"}
+        params["exchange"] = exchange
+        params["security_type"] = security_type
+        params["indicator_category"] = indicator_category
+        params["date_start"] = date_start
+        params["date_end"] = date_end
+        if master_id:
+            params["master_id"] = master_id
+        if indicator_id:
+            params["indicator_id"] = indicator_id
+        main_request = requests.get(self.base_url + "/get_data",params=params)
         try:
-            indicator_category_id = self.get_indicator_category_id(lookup=indicator)
+            return pd.read_csv(StringIO(main_request.text))
         except:
-            indicator_category_id = None
-        if indicator_category_id == None:
-            raise ValueError("did not find indicator")
-        main_symbol_object = {}
-        for temp_symbol in symbol:
-            symbol_param = {"lookup":temp_symbol.split(":")[-1],"format":"json"}
-            response = requests.get(self.base_url + "/get_master" ,params=symbol_param)
-            symbol_data = response.json()
-            for temp_symbol_data in symbol_data:
-                exchange_id = temp_symbol_data["exchange_id"]
-                security_type_id = temp_symbol_data["security_type_id"]
-                master_id = temp_symbol_data["master_id"]
-                exchange_code = temp_symbol_data["exchange_code"]
-                symbol_code = temp_symbol_data["symbol"]
-                main_symbol_object[temp_symbol] = {"exchange_id":exchange_id,"security_type_id":security_type_id,"master_id":master_id,"exchange_code":exchange_code,"symbol_code":symbol_code}
-        return_df = pd.DataFrame(columns = ['master_id',"indicator_id",'value','data_type','ts_date','ts_hour'])
-        for main_symbol in main_symbol_object:
-            params = {"exchange":main_symbol_object[main_symbol]["exchange_id"],"security_type":main_symbol_object[main_symbol]["security_type_id"],"indicator_category":indicator_category_id,"date_start":date_start,"format":"csv"}
-            params["date_end"] = date_end
-            params["master_id"] = main_symbol_object[main_symbol]["master_id"]
-            main_request = requests.get(self.base_url + "/get_data",params=params)
-            try:
-                symbol_df = pd.read_csv(StringIO(main_request.text))
-                return_df = return_df.append(symbol_df)
-            except:
-                pass
-        return return_df
+            raise ValueError("could not load dataframe")
 
-    def getOHLCVData(self,symbol,date_start,date_end):
-        self.validate_date(date_start)
-        self.validate_date(date_end)
-        start_date_object = datetime.datetime.strptime(date_start, '%Y-%m-%d,%H:%M:%S')
-        end_date_object = datetime.datetime.strptime(date_end, '%Y-%m-%d,%H:%M:%S')
-        if end_date_object < start_date_object:
-            raise ValueError("end date is before start date")
-        main_symbol_object = {}
-        for temp_symbol in symbol:
-            symbol_param = {"lookup":temp_symbol.split(":")[-1],"format":"json"}
-            response = requests.get(self.base_url + "/get_master" ,params=symbol_param)
-            symbol_data = response.json()
-            for temp_symbol_data in symbol_data:
-                exchange_id = temp_symbol_data["exchange_id"]
-                security_type_id = temp_symbol_data["security_type_id"]
-                master_id = temp_symbol_data["master_id"]
-                exchange_code = temp_symbol_data["exchange_code"]
-                symbol_code = temp_symbol_data["symbol"]
-                main_symbol_object[temp_symbol] = {"exchange_id":exchange_id,"security_type_id":security_type_id,"master_id":master_id,"exchange_code":exchange_code,"symbol_code":symbol_code}
-        return_df = pd.DataFrame(columns = ['datetime',"exchange",'symbol','open','high','low','close','volume'])
-        for main_symbol in main_symbol_object:
-            params = {"exchange":main_symbol_object[main_symbol]["exchange_id"],"security_type":main_symbol_object[main_symbol]["security_type_id"],"indicator_category":1,"date_start":date_start,"format":"csv"}
-            params["date_end"] = date_end
-            params["master_id"] = main_symbol_object[main_symbol]["master_id"]
-            params["indicator_id"] = "371,373,375,377,379"
-            main_request = requests.get(self.base_url + "/get_data",params=params)
-            try:
-                return_symbol = main_symbol_object[main_symbol]["symbol_code"]
-                return_exchange = main_symbol_object[main_symbol]["exchange_code"]
-                symbol_df = pd.read_csv(StringIO(main_request.text))
-                symbol_df["new_date"] = symbol_df["ts_date"] + " " + symbol_df["ts_hour"]
-                for ticker_group_name, ticker_group_df in symbol_df.groupby("new_date"):
+    def getOHLCVData(self,exchange="",security_type="",date_start="",date_end="",master_id=""):
+        df = self.getdata(exchange,security_type,1,date_start,date_end,master_id=master_id)
+        return_df = pd.DataFrame(columns = ['datetime',"exchange_id",'master_id','open','high','low','close','volume'])
+        try:
+            df["new_date"] = df["ts_date"] + " " + df["ts_hour"]
+            for group_name, group_df in df.groupby("master_id"):
+                ticker_df = group_df.copy()
+                for ticker_group_name, ticker_group_df in ticker_df.groupby("new_date"):
                     datetime_df = ticker_group_df.copy()
                     open_price = datetime_df[datetime_df["indicator_id"] == 377]
                     if open_price.empty == False:
@@ -198,27 +153,30 @@ class client:
                         close_value = close_price["value"].unique()[0]
                     else:
                         close_value = np.nan
-                    return_entry = [datetime_df['new_date'].unique()[0],return_exchange,return_symbol,open_value,high_value,low_value,close_value,volume_value]
+                    return_entry = [datetime_df['new_date'].unique()[0],exchange,datetime_df['master_id'].unique()[0],open_value,high_value,low_value,close_value,volume_value]
                     return_df.loc[len(return_df)] = return_entry
-            except:
-                pass
-        return return_df
+            return return_df
+        except Exception as e:
+            raise ValueError(e)
 
     def export_df(self,df,file_format,path): # export a dataframe
         if not isinstance(df, pd.DataFrame):
             raise ValueError("Please provide a dataframe")
-        if file_format == 'csv':
-            if ".csv" == path[-4:]:
-                df.to_csv(path, index = None, header=True)
-            else:
-                df.to_csv(path + ".csv", index = None, header=True)
-        if file_format == 'excel':
-            if ".xlsx" == path[-5:]:
-                df.to_excel(path, index = None, header=True)
-            else:
-                df.to_excel(path + ".xlsx", index = None, header=True)
-        if file_format == 'json' or file_format == "JSON":
-            if ".json" == path[-5:]:
-                df.to_json(path, orient='records')
-            else:
-                df.to_json(path + ".json", orient='records')
+        try:
+            if file_format == 'csv':
+                if ".csv" == path[-4:]:
+                    df.to_csv(path, index = None, header=True)
+                else:
+                    df.to_csv(path + ".csv", index = None, header=True)
+            if file_format == 'excel':
+                if ".xlsx" == path[-5:]:
+                    df.to_excel(path, index = None, header=True)
+                else:
+                    df.to_excel(path + ".xlsx", index = None, header=True)
+            if file_format == 'json' or file_format == "JSON":
+                if ".json" == path[-5:]:
+                    df.to_json(path, orient='records')
+                else:
+                    df.to_json(path + ".json", orient='records')
+        except Exception as e:
+            raise ValueError(e)
